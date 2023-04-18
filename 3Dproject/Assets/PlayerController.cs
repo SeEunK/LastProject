213518d6082644cfeb59cbb686f524ab64ff0c61
KSE <silverami.k@gmail.com>
2023-04-18 21:30:13 +0900

    [k - player jump]

--- 3Dproject/Assets/PlayerController.cs
+++ 3Dproject/Assets/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -12,23 +13,32 @@ public class PlayerController : MonoBehaviour {
     public float mRunSpeed = 10.0f;
     public float mTurnSpeed = 40.0f;
     private Vector3 mMoveDirection = Vector3.zero;
-    public float gravity = 20.0f;
+    public float gravity = Physics.gravity.magnitude;
     private bool mIsRun  = false;
     public bool mIsJump = false;
-    public float mJumpSpeed = 600.0f;
- 
+    public float mJumpForce = 10.0f;
+    private float mFallMultiplier = 2.5f;
 
+
+    private NavMeshAgent mAgent;
+
+
+   
+
+   
 
 
     void Awake()
     {
-
+        mAgent = GetComponent<NavMeshAgent>();
+        
         mAnim = gameObject.GetComponent<Animator>();
         mRigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        
 
         mVerticalAxis = Input.GetAxis("Vertical"); // up & down 
         mHorizontalAxis = Input.GetAxis("Horizontal"); // left & right
@@ -54,22 +64,59 @@ public class PlayerController : MonoBehaviour {
 
 
 
-        Jump();
+        if (Input.GetButtonDown("Jump") && !mIsJump)
+        {
+            //mAgent.enabled = false;
+            //mRigidbody.velocity = new Vector3(mRigidbody.velocity.x, 0, mRigidbody.velocity.z);
+            //float jumpVelocity = Mathf.Sqrt(2*jump)
+            mRigidbody.AddForce(Vector3.up * mJumpForce, ForceMode.Impulse);
+
+
+            Debug.Log("Jump");
+            mAnim.SetBool("IsJump", true);
+            mAnim.SetTrigger("Jump");
+            mIsJump = true;
+
+        }
+        
+        if(mIsJump)
+        {
+            if (transform.position.y < mJumpForce)
+            {
+                transform.position += Vector3.up * mFallMultiplier * Physics.gravity.magnitude * Time.deltaTime;
+                mAgent.enabled = false;
+            }
+            else
+            {
+                mAgent.enabled = true;
+            }
+        }
 
         
-      
-        mMoveDirection.y -= gravity * Time.deltaTime;
+        if(Input.GetMouseButtonDown(0))
+        {
+            mAgent.enabled = true;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit)) 
+            {
+                mAgent.SetDestination(hit.point);   
+            }
+        }
 
-  
+        //mMoveDirection.y -= gravity * Time.deltaTime;
 
     }
+
+    
+
 
     private void Jump()
     {
         if (Input.GetButtonDown("Jump") && !mIsJump)
         {
             mRigidbody.velocity = Vector3.zero;
-            mRigidbody.AddForce(Vector3.up * mJumpSpeed, ForceMode.Impulse);
+            mRigidbody.AddForce(transform.up * mJumpForce, ForceMode.Impulse);
 
 
             //mMoveDirection.y = mJumpSpeed;
@@ -83,8 +130,10 @@ public class PlayerController : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
+        mAgent.enabled = true;
         if (collision.gameObject.CompareTag("Ground"))
         {
+           
             mAnim.SetBool("IsJump", false);
             mIsJump = false;
         }
